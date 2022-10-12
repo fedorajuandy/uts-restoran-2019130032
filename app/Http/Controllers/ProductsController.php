4990e0087243acc6f53e04c449d78e2299514f39
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-10-12 23:51:27 +0700

    controllers noted, about done

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,6 +14,42 @@ class ProductsController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('/products');
+        $carousels = [
+            [
+                "url" => "https://source.unsplash.com/G-jo31ESuRE/100px180",
+                "title" => "Everybody E.",
+                "text" => "Restaurant owner"
+            ],
+            [
+                "url" => "https://source.unsplash.com/v3OlBE6-fhU/100px180",
+                "title" => "Somebody E.",
+                "text" => "Executive chef"
+            ],
+            [
+                "url" => "https://source.unsplash.com/0Zx1bDv5BNY/100px180",
+                "title" => "Anybody E.",
+                "text" => "General manager"
+            ]
+        ];
+
+        $cards = [
+            [
+                "url" => "https://source.unsplash.com/G-jo31ESuRE/100px180",
+                "title" => "Everybody E.",
+                "text" => "Restaurant owner"
+            ],
+            [
+                "url" => "https://source.unsplash.com/v3OlBE6-fhU/100px180",
+                "title" => "Somebody E.",
+                "text" => "Executive chef"
+            ],
+            [
+                "url" => "https://source.unsplash.com/0Zx1bDv5BNY/100px180",
+                "title" => "Anybody E.",
+                "text" => "General manager"
+            ]
+        ];
+
+        return view('/products', compact('carousels', 'cards'));
     }
 }
--- app/Http/Controllers/TestimonialsController.php
+++ app/Http/Controllers/TestimonialsController.php
@@ -14,6 +14,24 @@ class TestimonialsController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('/testimonials');
+        $cards = [
+            [
+                "url" => "https://source.unsplash.com/G-jo31ESuRE/100px180",
+                "title" => "Everybody E.",
+                "text" => "Restaurant owner"
+            ],
+            [
+                "url" => "https://source.unsplash.com/v3OlBE6-fhU/100px180",
+                "title" => "Somebody E.",
+                "text" => "Executive chef"
+            ],
+            [
+                "url" => "https://source.unsplash.com/0Zx1bDv5BNY/100px180",
+                "title" => "Anybody E.",
+                "text" => "General manager"
+            ]
+        ];
+
+        return view('/testimonials', compact('cards'));
     }
 }
